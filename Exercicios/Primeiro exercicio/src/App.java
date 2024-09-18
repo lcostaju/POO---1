56809463c37cefdb59eeb6e571f6d23e6ca54a19
lcostaju <luciocj_eng@outlook.com>
2024-09-18 20:33:35 -0300

    finalizando primeiro exercicio

--- Exercicios/Primeiro exercicio/src/App.java
+++ Exercicios/Primeiro exercicio/src/App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = le();
+        exibe(par(numero));
+    }
+    //1 -função que retorna valor e não tem parametros
+    public static int le(){
+        Scanner s = new Scanner(System.in);        
+        System.out.print("Digite um valor Inteiro positivo:");
+        int valor = s.nextInt();
+        while(valor<0){
+            restricao();
+            System.out.print("Digite um valor Inteiro positivo:");
+            valor = s.nextInt();
+        }
+        s.close();
+        return valor;
+    }
+    //2 - função que não retorna valor e não tem parametros
+    public static void restricao(){
+        System.out.println("Valor digitado não é inteiro");
+    }
+    //3 - função que não retorna valor e tem parametros
+    public static void exibe(boolean par){
+        if (par) {
+            System.out.println("O numero digitado é par");
+        }else{
+            System.out.println("O numero digitado é impar");
+        }
+    }
+    //4 - função que retorna valor e tem parametros
+    public static boolean par(int numero){
+        boolean par = false;
+        if (numero %2 == 0) {
+            par = true;
+        }
+        return par;
     }
 }
